273701476bd7bc8d22acab4e25a71d60cb1e3511
vallabh_ghanwar <vallabhg@vlinkd.com.au>
2021-05-09 22:42:00 +0530

    update our menu-item to navigate with Router HOC(Higher order component)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
-import './App.css';
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </switch>
     </div>
   );
 }
